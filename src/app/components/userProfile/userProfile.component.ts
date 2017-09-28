34c4cb3ad8ca4a2bc3d6bd30336642cb0bdfd250
IvanovaNataly <letter4natalia@gmail.com>
2017-09-28 13:11:28 +0300

    queryParams

--- src/app/components/userProfile/userProfile.component.ts
+++ src/app/components/userProfile/userProfile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'user-profile',
@@ -8,5 +9,28 @@ import { Component } from '@angular/core';
 })
 
 export default class UserProfileComponent {
+    sub: any;
+    page: any;
 
+    constructor(private route: ActivatedRoute, private router: Router) {
+        console.log("UserProfileComponent");
+    }
+
+    ngOnInit() {
+        this.sub = this.route
+            .queryParams
+            .subscribe(params => {
+                // Defaults to 0 if no query param provided.
+                this.page = +params['page'] //|| 0;
+            });
+        console.log(this.page);
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    nextPage() {
+        this.router.navigate(['product-list'], { queryParams: { page: this.page + 1 } });
+    }
 }
